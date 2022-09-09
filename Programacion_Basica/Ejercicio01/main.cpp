6b92674113b79cf782f1b34f021a93957015179a
Sigfrido <sigfrido_o@ardilluda.com.mx>
2022-09-09 04:14:24 -0500

    actualizando

--- Programacion_Basica/Ejercicio01/main.cpp
+++ Programacion_Basica/Ejercicio01/main.cpp
@@ -12,20 +12,35 @@ void nuevoTema (char tema[]) {
 int main() {
     nuevoTema("variables");
     
-    int edad;
+    int entoreoConSigno;
+    unsigned int numeroSinSigno;
     float temperatura;
     char caracter;
     char nombre[] = "Yahir";
+    bool tieneAuto = true;
     
-    edad = 21.5;
+    entoreoConSigno = 2147483649;
+    numeroSinSigno = 2147483649;
     temperatura = 21.12;
     caracter = 'A';
     
     
-    cout << "edad: " << edad <<endl;
+    cout << "entoreoConSigno: " << entoreoConSigno <<endl;
+    cout << "numeroSinSigno: " << numeroSinSigno <<endl;
     cout << "temperatura : " << temperatura << endl;
     cout << "caracter: "  << caracter << endl;
     cout << "nombre: "  << nombre << endl;
+    cout << "tieneAuto: "  << tieneAuto << endl;
+    
+    
+    nuevoTema("comparación");
+    
+    cout<< "3 < 5" << " : " << (3 < 5)  << endl;
+    cout<< "3 > 5" << " : " << (3 > 5)  << endl;
+    cout<< "3 == 5" << " : " << (3 == 5)  << endl;
+    
+    cout<< "3 <= 3" << " : " << (3 <= 3)  << endl;
+    cout<< "3 >= 4" << " : " << (3 >= 4)  << endl;
     
 
     return 0;
